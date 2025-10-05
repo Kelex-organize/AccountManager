70c8d81dbe9feaffde426ac7c4ce5d06c6f8babe
kelex-organize <emanuel.laguna.organize@gmail.com>
2025-10-05 20:43:47 -0300

    Template

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="p-10 text-center text-2xl text-blue-500">¡Hola Tailwind v4 + Vue + Tauri!</div>
+</template>
+
+<style scoped></style>
